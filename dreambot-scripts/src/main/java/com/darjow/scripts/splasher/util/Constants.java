f01cea539649eb649a43e7fa9560aa23242af4a5
darjow <dariobronders123@hotmail.com>
2023-08-28 21:22:17 +0200

    cleanup

--- dreambot-scripts/src/main/java/com/darjow/scripts/splasher/util/Constants.java
+++ dreambot-scripts/src/main/java/com/darjow/scripts/splasher/util/Constants.java
@@ -3,11 +3,7 @@ package com.darjow.scripts.splasher.util;
 public class Constants {
     public static class ItemIDs{
         public static final int COINS = 995;
-        public static final int FIRE_RUNE = 554;
-        public static final int AIR_RUNE = 556;
-        public static final int MIND_RUNE = 558;
         public static final int CURSED_GOBLIN_STAFF = 11709;
-
     }
 
 }
